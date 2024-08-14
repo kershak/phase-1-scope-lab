60c68d0e104a56c8b8ed64d660f5b3985f9e3d3e
NillsLopez <yakusaz17@gmail.com>
2024-08-14 17:12:26 -0400

    completed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Write your solution in this file!
+/* Write your solution in this file!
 var customerName = 'bob'
 const leastFavoriteCustomer = 'juan'
 
@@ -12,6 +12,26 @@ function setBestCustomer(){
 
 function overwriteBestCustomer() {
     bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'juan'
+}
+    */
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Pedro';
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase(0);
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob';
 }
 
 function changeLeastFavoriteCustomer () {
